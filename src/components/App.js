f5475ab1a12b2b84c19f39abe172739be8332188
MANOJ <rajarapumanoj1999@gmail.com>
2026-09-19 16:33:33 +0530

    Implement lift state up login flow

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import Login from "./Login";
+
 
 const App = () => {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  function handleLogin(){
+    setIsLoggedIn(true)
+  }
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Parent Component</h1>
+        <Login isLoggedIn={isLoggedIn} onLogin={handleLogin}/>
     </div>
   )
 }
-
-export default App
+export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = ({isLoggedIn, onLogin}) => {
 
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
   function handleSubmit(e){
     e.preventDefault();
+    if(userName.trim() === "" || password.trim()===""){
+        return;
+    }
     onLogin()
     
     
@@ -15,10 +21,10 @@ const Login = ({isLoggedIn, onLogin}) => {
             <h2>You are Logged in!</h2>
         ):(
         <form onSubmit={handleSubmit}>
-            Username : <input type='text'/>
+            Username : <input type='text' value={userName} onChange={(e)=> setUserName(e.target.value)} required/>
             <br/>
             <br/>
-            Password : <input type="password"/>
+            Password : <input type="password" value={password} onChange={(e)=> setPassword(e.target.value)} required/>
             <br/>
             <br/>
             <button type='submit'>Login</button>
